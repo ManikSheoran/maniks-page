3d7e338bd309fa540cef352b3578cd6fe45c6f78
MANIK <2022itb100.manik@students.iiests.ac.in>
2024-06-30 11:17:42 +0530

    env

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
@@ -11,6 +14,11 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
